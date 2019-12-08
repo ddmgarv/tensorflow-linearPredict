f4e5d86264b14c04ee104714755575ae19d07cce
Gustavo <garv1998131@gmail.com>
2019-12-07 22:12:02 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import * as tf from "@tensorflow/tfjs";
+const App = () => {
+  const [value, setValue] = useState(0);
+  let linearModel = tf.Sequential;
+  let prediction;
 
-function App() {
+  const trainNewModel = async () => {
+    // Define a model for linear regression
+    linearModel = tf.sequential();
+    linearModel.add(
+      tf.layers.dense({
+        units: 1,
+        inputShape: [1]
+      })
+    );
+    // Preparing model for training, specify loss and optimizer
+    linearModel.compile({
+      loss: "meanSquaredError",
+      optimizer: "sgd"
+    });
+
+    // Training data
+    const xs = tf.tensor1d([3.2, 4.4, 5.5]);
+    const ys = tf.tensor1d([1.6, 2.7, 3.5]);
+
+    await linearModel.fit(xs, ys);
+
+    console.log("Model trained");
+  };
+
+  const predict = number => {
+    const output = linearModel.predict(tf.tensor2d([number], [1, 1]));
+    prediction = Array.from(output.dataSync())[0];
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{ marginTop: "15px", display: "flex", justifyContent: "center" }}
+    >
+      <input
+        type="text"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+        name=""
+        id=""
+      />
     </div>
   );
-}
+};
 
 export default App;
