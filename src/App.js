37be786fb1f815967ef3e8cb393a88d3e052e29f
Gustavo <garv1998131@gmail.com>
2019-12-08 21:59:52 -0300

    added createmodel, normalize data, pass in data to the model and train it...

--- src/App.js
+++ src/App.js
@@ -1,31 +1,56 @@
 import React, { useState, useEffect } from "react";
+import * as tfvis from "@tensorflow/tfjs-vis";
 import { predictVal } from "./Model/LinearRegression";
+import { getData } from "./LoadData/getData";
+import {
+  loadParameters,
+  createModel,
+  convertToTensor,
+  trainModel
+} from "./Model/2DPrediction";
 
 const App = () => {
   const [inputVal, setInputVal] = useState(0);
   const [predictedVal, setPredictedVal] = useState();
 
-  useEffect(() => {
-    setPredictedVal(predictVal(inputVal));
-  }, [inputVal]);
+  // useEffect(() => {
+  //   setPredictedVal(predictVal(inputVal));
+  // }, [inputVal]);
 
+  useEffect(() => {
+    const loadModel = async () => {
+      const imgData = await getData();
+      loadParameters(imgData);
+      const model = await createModel();
+      tfvis.show.modelSummary({ name: "Model Summary" }, model);
+      const tensorData = await convertToTensor(imgData);
+      const trainedModel = await trainModel(
+        model,
+        tensorData.inputs,
+        tensorData.labels
+      );
+      console.log("Model trained!");
+    };
+    loadModel();
+  }, []);
   return (
-    <div
-      style={{
-        marginTop: "15px",
-        display: "flex",
-        justifyContent: "center"
-      }}
-    >
-      <input
-        type="text"
-        value={inputVal}
-        onChange={e => setInputVal(e.target.value)}
-        name=""
-        id=""
-      />
-      <div>{predictedVal}</div>
-    </div>
+    // <div
+    //   style={{
+    //     marginTop: "15px",
+    //     display: "flex",
+    //     justifyContent: "center"
+    //   }}
+    // >
+    //   <input
+    //     type="text"
+    //     value={inputVal}
+    //     onChange={e => setInputVal(e.target.value)}
+    //     name=""
+    //     id=""
+    //   />
+    //   <div>{predictedVal}</div>
+    // </div>
+    <div></div>
   );
 };
 
---
+++ src/LoadData/getData.js
@@ -0,0 +1,18 @@
+/**
+ * Get the car data reduced to just the variables we are interested
+ * and cleaned of missing data.
+ */
+export async function getData() {
+  const carsDataReq = await fetch(
+    "https://storage.googleapis.com/tfjs-tutorials/carsData.json"
+  );
+  const carsData = await carsDataReq.json();
+  const cleaned = carsData
+    .map(car => ({
+      mpg: car.Miles_per_Gallon,
+      horsepower: car.Horsepower
+    }))
+    .filter(car => car.mpg != null && car.horsepower != null);
+
+  return cleaned;
+}
